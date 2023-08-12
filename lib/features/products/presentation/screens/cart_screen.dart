48883ed2291c36c77ee49cb9ccc91fcd8478c7d5
I.ATIF <i.atif@diyarme.com>
2023-08-12 11:45:07 +0300

    reformat code of cart items

--- lib/features/products/presentation/screens/cart_screen.dart
+++ lib/features/products/presentation/screens/cart_screen.dart
@@ -8,6 +8,7 @@ import 'package:task/core/utils/app_strings.dart';
 import 'package:task/core/utils/palette.dart';
 import 'package:task/core/utils/styles.dart';
 import 'package:task/features/products/presentation/managers/products_cubit/products_cubit.dart';
+import 'package:task/features/products/presentation/widgets/cart_item_widget.dart';
 import 'package:task/features/products/presentation/widgets/custom_button.dart';
 
 class CartScreen extends StatefulWidget {
@@ -87,166 +88,10 @@ class _CartScreenState extends State<CartScreen> {
                         EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
                     itemCount: productsCubit.cartList.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: EdgeInsets.only(bottom: 24.h),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(15.r),
-                              ),
-                              child: CachedNetworkImage(
-                                imageUrl:
-                                    productsCubit.cartList[index].thumbnail!,
-                                height: 150.h,
-                                width: 120.w,
-                                fit: BoxFit.cover,
-                                progressIndicatorBuilder: (
-                                  BuildContext context,
-                                  String url,
-                                  DownloadProgress downloadProgress,
-                                ) {
-                                  return Center(
-                                    child: SizedBox(
-                                      width: 32.w,
-                                      height: 32.h,
-                                      child: CircularProgressIndicator(
-                                        color: Palette.kLightBlue,
-                                        value: downloadProgress.progress,
-                                      ),
-                                    ),
-                                  );
-                                },
-                                errorWidget:
-                                    (BuildContext context, String url, error) =>
-                                        const Icon(Icons.error),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 16.w,
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  SizedBox(
-                                    height: 8.h,
-                                  ),
-                                  Text(productsCubit.cartList[index].title!,
-                                      textAlign: TextAlign.start,
-                                      style: Styles.titleStyle20.copyWith(
-                                        color: Palette.kBlack,
-                                        fontWeight: FontWeight.w700,
-                                      )),
-                                  SizedBox(
-                                    height: 8.h,
-                                  ),
-                                  FittedBox(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      mainAxisSize: MainAxisSize.max,
-                                      children: [
-                                        Text(
-                                            "\$${(productsCubit.cartList[index].price! - (productsCubit.cartList[index].price! * productsCubit.cartList[index].discountPercentage!) / 100).toStringAsFixed(2)}",
-                                            style: Styles.titleStyle18.copyWith(
-                                                color: Palette.kBlack,
-                                                fontWeight: FontWeight.w500)),
-                                        SizedBox(
-                                          width: 15.w,
-                                        ),
-                                        Text(
-                                            "\$${productsCubit.cartList[index].price!}",
-                                            style: Styles.titleStyle18.copyWith(
-                                                color: Palette.greyBorder,
-                                                decoration:
-                                                    TextDecoration.lineThrough,
-                                                decorationColor:
-                                                    Palette.greyBorder,
-                                                decorationStyle:
-                                                    TextDecorationStyle.solid,
-                                                decorationThickness: 3.w,
-                                                fontWeight: FontWeight.w500)),
-                                        SizedBox(
-                                          width: 15.w,
-                                        ),
-                                        Text(
-                                            "${productsCubit.cartList[index].discountPercentage!}% off",
-                                            style: Styles.titleStyle18.copyWith(
-                                                color: Palette.greyBorder,
-                                                fontWeight: FontWeight.w500)),
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 16.h,
-                                  ),
-                                  Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      InkWell(
-                                        onTap: () {
-                                          productsCubit
-                                              .decreaseCountOfCartItem(index);
-                                        },
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 8.h, vertical: 8.w),
-                                          decoration: BoxDecoration(
-                                            color: Palette.kBlack,
-                                            border: Border.all(
-                                                color: Palette.greyBorder),
-                                            borderRadius:
-                                                BorderRadius.circular(10.r),
-                                          ),
-                                          child: Icon(
-                                            Icons.remove,
-                                            color: Palette.kWhite,
-                                            size: 32.w,
-                                          ),
-                                        ),
-                                      ),
-                                      Text(
-                                          "${productsCubit.cartList[index].count}",
-                                          style: Styles.titleStyle18.copyWith(
-                                              color: Palette.greyBorder,
-                                              fontWeight: FontWeight.w500)),
-                                      InkWell(
-                                        onTap: () {
-                                          productsCubit
-                                              .increaseCountOfCartItem(index);
-                                        },
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 8.h, vertical: 8.w),
-                                          decoration: BoxDecoration(
-                                            color: Palette.kBlack,
-                                            border: Border.all(
-                                                color: Palette.greyBorder),
-                                            borderRadius:
-                                                BorderRadius.circular(10.r),
-                                          ),
-                                          child: Icon(
-                                            Icons.add,
-                                            color: Palette.kWhite,
-                                            size: 32.w,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                      return CartItem(
+                        product: productsCubit.cartList[index],
+                        productsCubit: productsCubit,
+                        index: index,
                       );
                     },
                   ),
@@ -255,8 +100,8 @@ class _CartScreenState extends State<CartScreen> {
                   ),
                   productsCubit.cartList.isNotEmpty
                       ? Padding(
-                        padding: EdgeInsets.only(bottom: 32.h),
-                        child: CustomElevatedButton(
+                          padding: EdgeInsets.only(bottom: 32.h),
+                          child: CustomElevatedButton(
                             onPressed: () {},
                             backgroundColor: Palette.kBlack,
                             borderColor: Palette.kBlack,
@@ -268,7 +113,7 @@ class _CartScreenState extends State<CartScreen> {
                             ),
                             textColor: Palette.kWhite,
                           ),
-                      )
+                        )
                       : const SizedBox.shrink(),
                 ],
               ),
---
+++ lib/features/products/presentation/widgets/cart_item_widget.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task/core/utils/palette.dart';
+import 'package:task/core/utils/styles.dart';
+import 'package:task/features/products/domain/entities/product_entity.dart';
+import 'package:task/features/products/presentation/managers/products_cubit/products_cubit.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({
+    super.key,
+    required this.productsCubit,
+    required this.product,
+    required this.index,
+  });
+  final ProductsCubit productsCubit;
+  final ProductEntity product;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 24.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.r),
+            ),
+            child: CachedNetworkImage(
+              imageUrl: product.thumbnail!,
+              height: 150.h,
+              width: 120.w,
+              fit: BoxFit.cover,
+              progressIndicatorBuilder: (
+                BuildContext context,
+                String url,
+                DownloadProgress downloadProgress,
+              ) {
+                return Center(
+                  child: SizedBox(
+                    width: 32.w,
+                    height: 32.h,
+                    child: CircularProgressIndicator(
+                      color: Palette.kLightBlue,
+                      value: downloadProgress.progress,
+                    ),
+                  ),
+                );
+              },
+              errorWidget: (BuildContext context, String url, error) =>
+                  const Icon(Icons.error),
+            ),
+          ),
+          SizedBox(
+            width: 16.w,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SizedBox(
+                  height: 8.h,
+                ),
+                Text(product.title!,
+                    textAlign: TextAlign.start,
+                    style: Styles.titleStyle20.copyWith(
+                      color: Palette.kBlack,
+                      fontWeight: FontWeight.w700,
+                    )),
+                SizedBox(
+                  height: 8.h,
+                ),
+                FittedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                          "\$${(product.price! - (product.price! * product.discountPercentage!) / 100).toStringAsFixed(2)}",
+                          style: Styles.titleStyle18.copyWith(
+                              color: Palette.kBlack,
+                              fontWeight: FontWeight.w500)),
+                      SizedBox(
+                        width: 15.w,
+                      ),
+                      Text("\$${product.price!}",
+                          style: Styles.titleStyle18.copyWith(
+                              color: Palette.greyBorder,
+                              decoration: TextDecoration.lineThrough,
+                              decorationColor: Palette.greyBorder,
+                              decorationStyle: TextDecorationStyle.solid,
+                              decorationThickness: 3.w,
+                              fontWeight: FontWeight.w500)),
+                      SizedBox(
+                        width: 15.w,
+                      ),
+                      Text("${product.discountPercentage!}% off",
+                          style: Styles.titleStyle18.copyWith(
+                              color: Palette.greyBorder,
+                              fontWeight: FontWeight.w500)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        productsCubit.decreaseCountOfCartItem(index);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.h, vertical: 8.w),
+                        decoration: BoxDecoration(
+                          color: Palette.kBlack,
+                          border: Border.all(color: Palette.greyBorder),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: Icon(
+                          Icons.remove,
+                          color: Palette.kWhite,
+                          size: 32.w,
+                        ),
+                      ),
+                    ),
+                    Text("${product.count}",
+                        style: Styles.titleStyle18.copyWith(
+                            color: Palette.greyBorder,
+                            fontWeight: FontWeight.w500)),
+                    InkWell(
+                      onTap: () {
+                        productsCubit.increaseCountOfCartItem(index);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.h, vertical: 8.w),
+                        decoration: BoxDecoration(
+                          color: Palette.kBlack,
+                          border: Border.all(color: Palette.greyBorder),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Palette.kWhite,
+                          size: 32.w,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
